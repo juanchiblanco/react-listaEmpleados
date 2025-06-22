d2b2ee3181c5258312f3ea15b62eb3f459cf4d6d
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-21 21:04:04 -0300

    retoques en el disenio

--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -4,15 +4,15 @@ import EmpleadoAvatar from "./EmpleadoAvatar";
 const EmpleadoRow = ({ empleado }) => {
   return (
     <ListGroup.Item>
-      <article className="row">
+      <article className="row my-3">
         <div className="col-12 col-md-3">
           <EmpleadoAvatar empleado={empleado}/>
         </div>
-        <div className="col-12 col-md-9">
+        <div className="col-12 col-md-9 text-center text-md-start mt-3">
           <h4>{empleado.fullName}</h4>
-          <div className="d-flex justify-content-start gap-4">
+          <div className="d-flex justify-content-center justify-content-md-start gap-4">
             <p>{empleado.title}</p>
-            <p className="bg-info">{empleado.department}</p>
+            <p className="bg-info px-2">{empleado.department}</p>
           </div>
         </div>
       </article>
